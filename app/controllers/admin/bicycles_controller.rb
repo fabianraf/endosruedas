d0c6e0111cc2f21594314d41df9c0d23a884d2ae
fabianraf <fabianraf@gmail.com>
2014-04-03 19:38:42 -0500

    Adding registration steps

--- app/controllers/admin/bicycles_controller.rb
+++ app/controllers/admin/bicycles_controller.rb
@@ -1,2 +1,3 @@
 class Admin::BicyclesController < ApplicationController
+	
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,18 +11,36 @@ class SessionsController < Devise::SessionsController
     if user_signed_in?
       session[:user_id] = current_user.id
       session[:logged_in_by_password] = 'yes'  
-      current_user.remember_me! if current_user.remember_token.blank?
+      current_user.remember_me! if current_user.remember_me.blank?
       cookies.permanent.signed[:auth_token] = {
-        :value => current_user.remember_token
+        :value => current_user.remember_me
         }
         
     end
   end
   
   def destroy
-    p "******************"
     super
     cookies[:auth_token] = nil
   end
   
+protected
+
+def after_inactive_sign_up_path_for(resource)
+  root_url(:protocol => 'http')
+end
+
+def after_sign_up_path_for(resource)
+  root_url(:protocol => 'http')
+end
+
+def after_update_path_for(resource)
+  edit_user_registration_url(:protocol => 'http')
+end
+
+
+  def after_sign_in_path_for(resource)
+    edit_user_registration_url
+  end
+
 end
