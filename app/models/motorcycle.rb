b4ef1353521f73a9f7c9635c3c30394e556a9d61
fabianraf <fabianraf@gmail.com>
2014-04-09 19:57:41 -0500

    motorcycles

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,5 +1,7 @@
 class  Motorcycle < Product
   	has_many :motorcycle_images, class_name: 'MotorcycleImage', dependent: :destroy
-    attr_accessible :mileage, :cylinder_capacity, :fuel_id  
+  	accepts_nested_attributes_for :motorcycle_images
+    	attr_accessible :mileage, :cylinder_capacity, :fuel_id , :motorcycle_images_attributes
+    	
     
 end
