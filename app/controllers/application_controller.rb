c370fa8a7daf944d7e5f126ff51dd862ea0e48c3
pblpzmn <elpablopazmino@gmail.com>
2014-05-04 20:52:13 -0500

    adding search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   layout 'home'
   protect_from_forgery
+  
+  before_filter :set_global_search_variable
+
+  def set_global_search_variable
+    @q = Product.search(params[:q])
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,25 @@
 class ProductsController < ApplicationController
 
 	def index
-		@bicycles = Bicycle.order_by_updated_at.order_by_type.paginate(:page => params[:page], :per_page => 20)
+		#@bicycles = Bicycle.order_by_updated_at.order_by_type.paginate(:page => params[:page], :per_page => 20)
+		@q = Bicycle.search(params[:q])
+    @bicycles = @q.result.order_by_updated_at.order_by_type.paginate(:page => params[:page])
 	end
   
   def show
     @bicycle = Bicycle.find(params[:id])
+  end
+  
+  
+  def search
+    case params[:product]
+      when "bicicleta"
+          index
+          render :index
+      when "motocicleta"
+          render :"admin/productos"
+      
+    end
   end
   
 end
