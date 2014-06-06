017ba9cf7d20a4a04247dff752589cc4552f8bd3
pblpzmn <elpablopazmino@gmail.com>
2014-06-06 00:17:50 -0500

    sendign email

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -3,7 +3,9 @@ class ContactUsController < ApplicationController
   def sendEmail
     puts params[:p]
     @name = params[:p][:name]
-    render "contact_us/sentEmail"
+    
+    ContactUs.send_email(params[:p]).deliver
+    render "contact_us/contact_us_thanks"
   end
   
 end
