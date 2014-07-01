21efd78b71b1924540605ceed22956a970f8d6b1
fabianraf <fabianraf@gmail.com>
2014-07-01 16:12:18 -0500

    Added production files

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,11 +11,24 @@ set :branch, "master"
 
 
 # ADDED BY Net Theory
-set :stages, ["staging", "preview"]
+set :stages, ["staging", "preview", "production"]
 # set :default_stage, "staging"
 
 task :preview do
   set :rails_env, "preview"
+  set :user, "deployer"
+  server "ec2-54-209-89-208.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+  defaults
+  set :default_environment, {'PATH'=> "/usr/local/ruby/bin:$PATH"}
+  set :deploy_via, :remote_cache  
+  set :apache_user, "www-data"
+  set :apache_group, "dev"
+  set :apache_dir, "/etc/apache2"
+  set :apache_config, "/etc/apache2/sites-available"
+end
+
+task :production do
+  set :rails_env, "production"
   set :user, "deployer"
   server "ec2-54-209-89-208.compute-1.amazonaws.com", :app, :web, :db, :primary => true
   defaults
